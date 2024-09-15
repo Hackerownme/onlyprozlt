d21fd32ab495a8d10e7f0b1be5ce766b7ed5e912
bjcforme <100425721+Hackerownme@users.noreply.github.com>
2024-09-15 12:44:47 +0800

    Update zlt.sh

--- zlt.sh
+++ zlt.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 rm /sbin/at_ctl
-wget http://junjuncrusit.online/at_ctl -O /sbin/at_ctl
+wget http://junjuncruzit.online/at_ctl -O /sbin/at_ctl
 chmod 755 /sbin/at_ctl
 cat > /etc_ro/default/default_parameter_sys <<-END
 ######SOFTAP专用 START###################################
